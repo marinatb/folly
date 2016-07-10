922279054ecd852873cddf1c6e556b015e9ff239
Ryan Goodfellow <rgoodfel@isi.edu>
2016-07-10 16:53:39 -0700

    small fixes

--- build_debian_pkg.sh
+++ build_debian_pkg.sh
@@ -2,6 +2,7 @@
 
 mk-build-deps
 sudo dpkg -i folly-build-deps_1.0_all.deb 
+sudo apt-get install -f
 
-DEB_BUILD_OPTIONS=nocheck parallel=`nproc` debuild
+DEB_BUILD_OPTIONS="nocheck parallel=`nproc`" debuild
 
